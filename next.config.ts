b9773e7266ee70f084c3862d1820137427708f4e
Rohit Singh Pal <rohit.pal7171@gmail.com>
2025-07-16 16:28:19 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,22 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.pdf$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            publicPath: '/_next/static/files',
+            outputPath: 'static/files',
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;

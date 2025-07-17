a22f5509ad593d10e55bdfada5a711eef0811cfa
Rohit Singh Pal <rohit.pal7171@gmail.com>
2025-07-17 11:00:07 +0000

    not getting reply

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -21,11 +21,6 @@ export async function askNpc(previousState: any, formData: FormData) {
   try {
     const stream = await npcResponseStream(validatedPrompt.data);
     
-    // Since we can't return a stream directly to useFormState, we'll handle streaming on the client
-    // For now, this action will just confirm the call was successful and the client will handle the stream.
-    // A more advanced implementation might use a separate mechanism to push stream data to the client.
-    // For this architecture, we will return the full response.
-    
     let fullResponse = "";
     for await (const chunk of stream) {
       fullResponse += chunk;
